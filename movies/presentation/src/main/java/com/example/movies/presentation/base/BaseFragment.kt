5a2d757a693825c33f6e7fd994e3642442b5d6a6
Giovanni Toledo <giovanni.toledo@globant.com>
2022-03-25 16:31:23 -0600

    Changed Composable to a different class

--- movies/presentation/src/main/java/com/example/movies/presentation/base/BaseFragment.kt
+++ movies/presentation/src/main/java/com/example/movies/presentation/base/BaseFragment.kt
@@ -4,9 +4,4 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 
-open class BaseFragment : Fragment() {
-
-    fun navigateDeepLinkRequest(directions: NavDirections) {
-        findNavController().navigate(directions)
-    }
-}
+open class BaseFragment : Fragment()
--- movies/presentation/src/main/java/com/example/movies/presentation/ui/moviesList/MoviesListView.kt
+++ movies/presentation/src/main/java/com/example/movies/presentation/ui/moviesList/MoviesListView.kt
@@ -2,40 +2,82 @@ package com.example.movies.presentation.ui.moviesList
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Scale
+import com.example.androidHelpers.compose.views.MovieLottieAnimation
 import com.example.movies.domain.entities.Movie
 
+@Composable
+fun MovieList(
+    movieList: List<Movie>?,
+    isLoadingVisible: Boolean = false,
+    onNavigate: (NavDirections) -> Unit
+) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        if (isLoadingVisible) {
+            MovieLottieAnimation()
+        } else {
+            movieList?.let {
+                LazyColumn {
+                    itemsIndexed(items = movieList) { _, item ->
+                        Box(modifier = Modifier
+                            .clickable {
+                                val navDirection =
+                                    MoviesListFragmentDirections.actionMoviesListFragmentToMovieDetailFragment(
+                                        movieId = 100
+                                    )
+                                onNavigate(navDirection)
+                            }
+                        ) {
+                            MovieItem(item)
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
 
 @Composable
-fun MovieItem(movie : Movie?) {
+fun MovieItem(movie: Movie?) {
     Card(
         modifier = Modifier
             .padding(8.dp, 4.dp)
             .fillMaxWidth()
             .height(100.dp),
         shape = RoundedCornerShape(8.dp),
-        elevation = 4.dp)
+        elevation = 4.dp
+    )
     {
         Surface() {
             Row(
                 Modifier
                     .padding(4.dp)
-                    .fillMaxSize()) {
+                    .fillMaxSize()
+            ) {
 
                 Image(
                     painter = rememberAsyncImagePainter(
@@ -43,7 +85,7 @@ fun MovieItem(movie : Movie?) {
                             .data(data = movie?.getCompleteUrlToDetails())
                             .apply(block = fun ImageRequest.Builder.() {
                                 scale(Scale.FILL)
-                              //  transformations(CircleCropTransformation())
+                                //  transformations(CircleCropTransformation())
                             }).build()
                     ),
                     contentDescription = movie?.overview,
